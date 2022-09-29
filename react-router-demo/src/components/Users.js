c6c0f5e130a49ee836b7d7235415fee43d2d6f31
Sasikant Biswal <Sasikant.Biswal@lntinfotech.com>
2022-09-29 17:22:24 +0530

    Added code for search routes through useSearchParams hooks

--- react-router-demo/src/components/Users.js
+++ react-router-demo/src/components/Users.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 const Users = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const showActiveUsers = searchParams.get('filter') === 'active';
+
     return (
         <div>
             <nav>
@@ -11,6 +15,17 @@ const Users = () => {
                 <Link to='3'>User 3</Link>
             </nav>
             <Outlet />
+            <div>
+                <button onClick={() => setSearchParams({filter: 'active'})}>Active Users</button>
+                <button onClick={() => setSearchParams({})}>Reset Filter</button>
+            </div>
+            {
+                showActiveUsers ? (
+                    <h2>Showing active users</h2>
+                ) : (
+                    <h2>Showing all users</h2>
+                )
+            }
         </div>
     )
 }
