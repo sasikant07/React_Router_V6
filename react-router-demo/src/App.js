7a0a5a2b29e80694c4ba66b9675c72330f3ddf55
Sasikant Biswal <Sasikant.Biswal@lntinfotech.com>
2022-09-29 15:31:18 +0530

    Added code to navigate prohramatically from components

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import OrderSummary from './components/OrderSummary';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path='order-summary' element={<OrderSummary />} />
       </Routes>
     </>
   );
